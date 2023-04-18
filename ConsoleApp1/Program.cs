168aacd04c3e411fb141afa51479de1531997ec2
Ismail Ahmadi <43436401+Ismail-ahmadi@users.noreply.github.com>
2023-04-17 20:08:30 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿public class ValueCollection<T> where T : struct
+{
+    private List<T> _values;
+
+    public ValueCollection()
+    {
+        _values = new List<T>();
+    }
+
+    public void Add(T value)
+    {
+        _values.Add(value);
+    }
+
+    public T GetItem(int index)
+    {
+        if (index < 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return _values[index];
+    }
+
+    public List<T> GetDescending()
+    {
+        List<T> sortedValues = new List<T>(_values);
+        sortedValues.Sort();
+        sortedValues.Reverse();
+        return sortedValues;
+    }
+}
