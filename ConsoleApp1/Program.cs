9f3ab30ebb7364a64d3233c7bf4013d2d1914d77
Ismail Ahmadi <43436401+Ismail-ahmadi@users.noreply.github.com>
2023-04-17 20:12:46 -0400

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,12 @@
-﻿public class ValueCollection<T> where T : struct
+﻿using System.ComponentModel;
+
+//Write a generic class that meets the following requirements:
+//1)   That constrains a developer to only instantiate the class based on value types
+//2)   Contains a private generic collection
+//3)   Has a method to add items to the private collection
+//4)   Has a method that will return an item from the list (e.g. item 3 or item 4 in the list)
+//5)   Has a method that returns a sorted collection with the items in descending order
+public class ValueCollection<T> where T : struct
 {
     private List<T> _values;
 
